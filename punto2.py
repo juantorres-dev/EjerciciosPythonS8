94558ea48c9cbfde38532a8a11b80b095fc61491
juan esteban torres ibarguen <jt158161@gmail.com>
2023-04-01 13:23:37 -0500

    Realizacion del tercer punto

---
+++ punto2.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+resultado = ""
+
+def numero (lista):
+    k = 1
+    i = 0
+    array = lista
+    for i in range(len(lista)):
+        if(lista[i]>lista[k]):
+            c = array[i]
+            d = array[k]
+            
+            array[i] = d
+            array[k] = c
+        k = k + 1
+        if(k == len(lista)):
+            return array
+            break
+numeros = [5,3,2,5,6,7,9]
+
+ciclo = len(numeros)
+for i in range(ciclo):
+    resultado = numero(numeros)
+
+print(resultado)
