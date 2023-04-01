df1186dfe50224746bd314bdad15161453e6065e
danalejos <sumigame98@gmail.com>
2023-04-01 11:35:06 -0500

    Se crea el tercer change

---
+++ lambda2.py
@@ -0,0 +1,4 @@
+sumar = lambda numero1,numero2: numero1+numero2
+
+resultado=sumar(2,2)
+print(f'la suma es: {resultado}')
---
+++ tradicional2.py
@@ -0,0 +1,5 @@
+def sumar (numero1,numero2):
+    return numero1 + numero2
+
+resultado = sumar (5,5)
+print(f'la suma es: {resultado}')
